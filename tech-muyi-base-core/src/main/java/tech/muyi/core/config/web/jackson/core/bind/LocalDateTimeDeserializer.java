1f491d190fd9dbfa2ba02f8a83b0ecf008a05e75
muyi <muyi_zcy@163.com>
2023-04-13 18:23:00 +0800

    LocalDateTime 反序列化

--- tech-muyi-base-core/src/main/java/tech/muyi/core/config/web/jackson/core/bind/LocalDateTimeDeserializer.java
+++ tech-muyi-base-core/src/main/java/tech/muyi/core/config/web/jackson/core/bind/LocalDateTimeDeserializer.java
@@ -1,25 +1,54 @@
 package tech.muyi.core.config.web.jackson.core.bind;
 
 import cn.hutool.core.date.LocalDateTimeUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
+import com.fasterxml.jackson.databind.ser.ContextualSerializer;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import tech.muyi.exception.MyException;
 import tech.muyi.exception.enumtype.CommonErrorCodeEnum;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
-public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> implements ContextualDeserializer {
     public static final LocalDateTimeDeserializer INSTANCE = new LocalDateTimeDeserializer();
+
+    public static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private JsonFormat format;
 
     @Override
     public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) {
         try {
-            LocalDateTime localDateTime = LocalDateTime.parse(p.getValueAsString());
-            return localDateTime;
-        }catch (Exception e){
+            if (format == null) {
+                return LocalDateTime.parse(p.getValueAsString(), formatter);
+            } else {
+                return LocalDateTime.parse(p.getValueAsString(), DateTimeFormatter.ofPattern(format.pattern()));
+            }
+        } catch (Exception e) {
             throw new MyException(CommonErrorCodeEnum.DESERIALIZATION_FAIL);
         }
+    }
+
+
+    @Override
+    public JsonDeserializer<?> createContextual(DeserializationContext deserializationContext, BeanProperty beanProperty) throws JsonMappingException {
+        if (beanProperty != null) {
+            if (Objects.equals(beanProperty.getType().getRawClass(), LocalDateTime.class)) {
+                JsonFormat jsonFormat = beanProperty.getAnnotation(JsonFormat.class);
+                if (jsonFormat != null) {
+                    return new LocalDateTimeDeserializer(format);
+                }
+            }
+        }
+        return this;
     }
 }
