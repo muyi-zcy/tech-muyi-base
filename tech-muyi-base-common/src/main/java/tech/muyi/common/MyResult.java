e9577bd50053d82a1d75425c05cda915767bdf51
muyi <muyi_zcy@163.com>
2023-09-27 21:46:40 +0800

    fix: 修复MyResult的success字段状态错误、修复ApplicationContextUtil无法自动加载

--- tech-muyi-base-common/src/main/java/tech/muyi/common/MyResult.java
+++ tech-muyi-base-common/src/main/java/tech/muyi/common/MyResult.java
@@ -1,8 +1,9 @@
 package tech.muyi.common;
 
-
+import lombok.Data;
 import tech.muyi.common.query.MyBaseQuery;
 import tech.muyi.exception.MyException;
+import tech.muyi.exception.enumtype.CommonErrorCodeEnum;
 
 import java.io.Serializable;
 
@@ -10,23 +11,22 @@ import java.io.Serializable;
  * @Author: muyi
  * @Date: 2021/1/3 21:27
  */
+@Data
 public class MyResult<T> implements Serializable {
-    private static final long serialVersionUID = 2994494589314183496L;
+    private static final long serialVersionUID = 1L;
+
     private boolean success;
     private T data;
     private MyBaseQuery query;
     private String code;
     private String message;
 
-    public MyResult() {
-    }
 
-
-    public static <T> MyResult ok(T data, MyBaseQuery query) {
-        MyResult<T> result = new MyResult();
+    public static <T> MyResult<T> ok(T data, MyBaseQuery query) {
+        MyResult<T> result = new MyResult<>();
         result.setData(data);
         result.setQuery(query);
-        result.isSuccess();
+        result.success = true;
         result.setCode("0");
         return result;
     }
@@ -40,51 +40,18 @@ public class MyResult<T> implements Serializable {
     }
 
     public static <T> MyResult<T> fail(String errCode, String errMsg) {
-        MyResult result = new MyResult();
-        result.isSuccess();
+        MyResult<T> result = new MyResult<>();
+        result.success = true;
         result.setCode(errCode);
         result.setMessage(errMsg);
         return result;
     }
 
-    public static <T> MyResult<T> fail(MyException e) {
-        return fail(e.getErrorCode(), e.getErrorMsg());
-    }
-
-    public boolean isSuccess() {
-        return this.success;
-    }
-
-
-    public T getData() {
-        return this.data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public MyBaseQuery getQuery() {
-        return this.query;
-    }
-
-    public void setQuery(MyBaseQuery query) {
-        this.query = query;
-    }
-
-    public String getCode() {
-        return this.code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return this.message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
+    public static <T> MyResult<T> fail(Throwable throwable) {
+        if (throwable instanceof MyException) {
+            MyException myException = (MyException) throwable;
+            return fail(myException.getErrorCode(), myException.getErrorMsg());
+        }
+        return fail(CommonErrorCodeEnum.UNKNOWN_EXCEPTION.getResultCode(), throwable.getMessage());
     }
 }
